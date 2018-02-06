bc881ab12c8307006ba695d508894532caf7d81d
Vincent Mwendwa <vrubben@outlook.com>
2018-02-06 13:36:04 +0000

    Update Ui.py

--- Ui.py
+++ Ui.py
@@ -1,9 +1,9 @@
 
 from PyQt5.QtWidgets import (
-							QMainWindow, QMenu, QSplitter, QAction, QTableWidget, QTableWidgetItem,
-							QGroupBox, QPushButton, QVBoxLayout, QHBoxLayout, QAbstractItemView, QSystemTrayIcon,
-							QMessageBox, QFormLayout, QLineEdit, QLabel, QDialog, QTextEdit, QFrame, QStatusBar
-							)
+			QMainWindow, QMenu, QSplitter, QAction, QTableWidget, QTableWidgetItem,
+			QGroupBox, QPushButton, QVBoxLayout, QHBoxLayout, QAbstractItemView, QSystemTrayIcon,
+			QMessageBox, QFormLayout, QLineEdit, QLabel, QDialog, QTextEdit, QFrame, QStatusBar
+			)
 from PyQt5.QtCore import (pyqtSlot, pyqtSignal, Qt, QEventLoop)
 from PyQt5.QtGui import (QPalette, QFont, QColor, QIcon)
 from subprocess import (Popen, PIPE)
