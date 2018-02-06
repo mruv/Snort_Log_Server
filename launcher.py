5482b805be2b80293ea7108c6b74c6d3778d1379
Vincent Mwendwa <vrubben@outlook.com>
2018-02-06 12:53:44 +0000

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,15 +1,26 @@
+
 from PyQt5.QtWidgets import QApplication
 import sys
+
 import Ui
 
 
+def launch(args):
+	""" Set up UI and start app
+	"""
+
+	app = QApplication(args)
+
+	win = Ui.MainWindow()
+	win.set_up_ui()
+	win.setVisible(True)
+
+	app.setQuitOnLastWindowClosed(False)
+	# start event loop
+	app.exec()
+
+
 if __name__ == '__main__':
-
-	app = QApplication(sys.argv)
-	win = Ui.MainView()
-	#initiaize and display UI
-	win.initUi()
-	#start server
-	win.startServer()
-
-	sys.exit(app.exec_())
+	""" App entry point
+	"""
+	launch(sys.argv)
